58d29b85e281e7a8af621147147d4ffa45c15ab1
seanevanlee <seanevanlee@gmail.com>
2023-01-05 13:19:10 -0800

    Able to partially CRUD an ATM

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,25 +2,18 @@ from django.urls import path
 from . import views
 
 
-
-
-
-
-
 # URLS GO HERE
 
 # Home page URL
 urlpatterns = [
     path('', views.home, name='home'),
 
-# About page 
+    # About page
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('atms/', views.atms_index, name='index'),
     path('atms/<int:atm_id>/', views.atms_detail, name='detail'),
     path('atms/create/', views.AtmCreate.as_view(), name='atms_create'),
-
-
-    
-
+    path('atms/<int:pk>/update/', views.AtmUpdate.as_view(), name='atms_update'),
+    path('atms/<int:pk>/delete/', views.AtmDelete.as_view(), name='atms_delete'),
 ]
